062a538cd7b09dc23a1af164d88f77cfea6465b8
Bryan Fields <bryan@bryanfields.net>
2020-12-15 02:14:43 -0500

    working model for first two section of a channel

--- csv.php
+++ csv.php
@@ -7,9 +7,12 @@ header( "content-type: application/xml; charset=UTF-8" );
 $xml = new DOMDocument( "1.0", "UTF-8" );
 
 // Create  elements.
-$root_import  = $xml->createElement( "import_export_doc" );
-    $version = $xml->createElement( "Version" );
-    $language = $xml->createElement( "Language" );
+$root_import  = $xml->createElement( 'import_export_doc' );
+$version = $xml->createElement('Version', 2);
+
+    
+    
+$language = $xml->createElement( 'Language', 'en' );
     $root_node = $xml->createElement( "Root" );
     $attr_root_features = new DOMAttr('ExportedAllFeatures','False');
     $root_node->setAttributeNode($attr_root_features);
@@ -24,14 +27,121 @@ $root_import  = $xml->createElement( "import_export_doc" );
     $attr_recset_features = new DOMAttr('Id','2059');
     $recsec_node->setAttributeNode($attr_recset_features);
     
-    $version->setAttributeNode('2');
+    // now make a Node under the Recset to hold the personality
+    
+    $Node = $xml->createElement( "Node" );
+    $Node_attr = new DOMAttr('Name','Conventional Personality');
+    $Node->setAttributeNode($Node_attr);
+    $Node_attr = new DOMAttr('ReferenceKey','TST 444.375');
+    $Node->setAttributeNode($Node_attr);
+    
+    // Now make the sections, all 14 of them
+    
+    
+        $Section = $xml->createElement( "Section" );
+        $Section_attr = new DOMAttr('Name','General');
+        $Section->setAttributeNode($Section_attr);
+        $Section_attr = new DOMAttr('id','10150');
+        $Section->setAttributeNode($Section_attr);
+    // now the fields in the section
+    
+            $Field_attr = new DOMAttr('Name','Conventional Personality Name');
+            $Field = $xml->createElement( "Field", 'TST 444.375' );
+            $Field->setAttributeNode($Field_attr);
+            $Section->appendChild($Field);
+    
+            $Field_attr = new DOMAttr('Name','DVRS Profile Selection');
+            $Field = $xml->createElement( "Field", '&lt;DVRS Disabled&gt;' );
+            $Field->setAttributeNode($Field_attr);
+            $Section->appendChild($Field);
+
+    
+     $Node->appendChild($Section);
+    // RX options section
+    $Section = $xml->createElement( "Section" );
+    $Section_attr = new DOMAttr('Name','Rx Options');
+    $Section->setAttributeNode($Section_attr);
+    $Section_attr = new DOMAttr('id','10136');
+    $Section->setAttributeNode($Section_attr);
+    // now the fields in the section
+    
+    $Field_attr = new DOMAttr('Name','Receive Only Personality');
+    $Field = $xml->createElement( "Field", 'False' );
+    $Field->setAttributeNode($Field_attr);
+    $Section->appendChild($Field);
+    
+    $Field_attr = new DOMAttr('Name','Rx Voice / Signal Type');
+    $Field = $xml->createElement( "Field", 'Mixed Mode' );
+    $Field->setAttributeNode($Field_attr);
+    $Section->appendChild($Field);
+    
+    $Field_attr = new DOMAttr('Name','Busy LED');
+    $Field = $xml->createElement( "Field", 'True' );
+    $Field->setAttributeNode($Field_attr);
+    $Section->appendChild($Field);
+    
+    $Field_attr = new DOMAttr('Name','Rx Unmute Delay (ms)');
+    $Field = $xml->createElement( "Field", '0' );
+    $Field->setAttributeNode($Field_attr);
+    $Section->appendChild($Field);
+    
+    $Field_attr = new DOMAttr('Name','Unmute / Mute Type');
+    $Field = $xml->createElement( "Field", '0' );
+    $Field->setAttributeNode($Field_attr);
+    $Section->appendChild($Field);
+    
+    $Field_attr = new DOMAttr('Name','Rx De-Emphasis');
+    $Field = $xml->createElement( "Field", 'True' );
+    $Field->setAttributeNode($Field_attr);
+    $Section->appendChild($Field);
+    
+    $Field_attr = new DOMAttr('Name','Squelch (Fine Tune)');
+    $Field = $xml->createElement( "Field", '6' );
+    $Field->setAttributeNode($Field_attr);
+    $Section->appendChild($Field);
+    
+    
+    $Field_attr = new DOMAttr('Name','HearClear');
+    $Field = $xml->createElement( "Field", 'Disabled' );
+    $Field->setAttributeNode($Field_attr);
+    $Section->appendChild($Field);
+
+    /*
+    $Field_attr = new DOMAttr('Name','');
+    $Field = $xml->createElement( "Field", '' );
+    $Field->setAttributeNode($Field_attr);
+    $Section->appendChild($Field);
+
+    
+    $Field_attr = new DOMAttr('Name','');
+    $Field = $xml->createElement( "Field", '' );
+    $Field->setAttributeNode($Field_attr);
+    $Section->appendChild($Field);
+*/
+    
+    
+    $Node->appendChild($Section);
+    
+    
+
+    $recsec_node->appendChild($Node);
+    
+    
+    
+    
+    
+
+    
+    
     
     $root_node->appendChild($recsec_node);
+    //var_dump ($version);
     $root_import->appendChild($version);
     $root_import->appendChild($language);
     $root_import->appendChild($root_node);
-    $xml->appendChild($root_node);
-    
+    $xml->appendChild($root_import);
+
+    $xml->formatOutput = TRUE;
 // Parse the XML.
 print $xml->saveXML();
 ?>
